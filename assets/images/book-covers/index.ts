b89f12c42c68fefc237f995d0159e7c9bb5db6f7
The Bit Cooler <thebitcooler@icloud.com>
2025-10-31 19:24:16 +0000

    chore: add book cover images

---
+++ assets/images/book-covers/index.ts
@@ -0,0 +1,11 @@
+export const bookCovers: Record<string, any> = {
+  MAIV: require('./MAIV.png'),
+  AKJV: require('./AKJV.png'),
+  ASV: require('./ASV.png'),
+  Darby: require('./Darby.png'),
+  Jubilee2000: require('./Jubilee2000.png'),
+  KJV: require('./KJV.png'),
+  NHEB: require('./NHEB.png'),
+  Webster: require('./Webster.png'),
+  YLT: require('./YLT.png'),
+};
