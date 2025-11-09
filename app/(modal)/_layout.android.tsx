fedf36b62f32d2ca92139bf6b9eaf1512866e3f8
The Bit Cooler <thebitcooler@icloud.com>
2025-11-09 21:29:48 +0000

    refactor(native): split modals layout into ios and android variants

---
+++ app/(modal)/_layout.android.tsx
@@ -0,0 +1,53 @@
+import { Stack } from 'expo-router';
+
+export default function ModalLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="bible-verse-explanation"
+        options={{
+          title: '',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="bible-verse-illustration"
+        options={{
+          title: 'Verse Illustration',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="bible-verse-versions"
+        options={{
+          title: '',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="new-bible-verse-translation"
+        options={{
+          title: '',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="similar-bible-verses"
+        options={{
+          title: '',
+          headerShown: true,
+        }}
+      />
+    </Stack>
+  );
+}
---
+++ app/(modal)/_layout.ios.tsx
@@ -0,0 +1,25 @@
+import { Stack } from 'expo-router';
+
+export default function ModalLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="bible-verse-illustration"
+        options={{
+          title: 'Verse Illustration',
+          headerShown: true,
+        }}
+      />
+    </Stack>
+  );
+}
