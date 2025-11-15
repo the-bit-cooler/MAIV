1db72bca5767af5707ba160ddd8d383ee32c51b3
The Bit Cooler <thebitcooler@icloud.com>
2025-11-15 20:56:32 +0000

    perf(util): compute list once on module load

--- utilities/get-bible-book-list.ts
+++ utilities/get-bible-book-list.ts
@@ -1,14 +1,16 @@
 import newTestamentBooks from '@/assets/data/new-testament.json';
 import oldTestamentBooks from '@/assets/data/old-testament.json';
 
-export const getBibleBookList = (): string[] => {
-  return getOldTestamentBibleBookList().concat(getNewTestamentBibleBookList());
-};
-
 export const getNewTestamentBibleBookList = (): string[] => {
   return newTestamentBooks;
 };
 
 export const getOldTestamentBibleBookList = (): string[] => {
   return oldTestamentBooks;
+};
+
+const bibleBooks = getOldTestamentBibleBookList().concat(getNewTestamentBibleBookList());
+
+export const getBibleBookList = (): string[] => {
+  return bibleBooks;
 };
